2a1e580cbcd95a05bb13d09c51b1636fdac19374
Jordan <Austin>
2019-11-18 17:29:50 -0600

    formatting

--- ais/modules/api/serializers.py
+++ ais/modules/api/serializers.py
@@ -11,9 +11,21 @@ class VesselSerializer(serializers.ModelSerializer):
     positions = serializers.SerializerMethodField("_get_positions")
     last_known = serializers.SerializerMethodField("_last_known_position")
     num_positions = serializers.SerializerMethodField("_num_positions")
+    distance_km = serializers.SerializerMethodField("_get_dist_covered")
+
     class Meta:
         model = Vessel
-        fields = ("id", "mmsi", "num_positions", "positions", "last_known", "dist_covered",)
+        fields = (
+            "id",
+            "mmsi",
+            "num_positions",
+            "positions",
+            "last_known",
+            "distance_km",
+        )
+
+    def _get_dist_covered(self, obj):
+        return "%s km" % round(obj.dist_covered / 1000, 2)
 
     def _num_positions(self, obj):
         return obj.num_positions
